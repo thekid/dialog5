ad0ccfb5a16e91247773f7bc8906d9bd9167ff60
friebe <friebe@d2cacbed-c0f6-0310-851a-9dad52fc623a>
2007-11-10 15:34:06 +0000

    - Move classes to project directory
    git-svn-id: svn+ssh://svn.xp-framework.net/home/svn/xpforge/trunk/projects/www/dialog@9856 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ classes/de/thekid/dialog/GroupByHourStrategy.class.php
@@ -0,0 +1,27 @@
+<?php
+/* This class is part of the XP framework
+ *
+ * $Id$
+ */
+
+  uses('de.thekid.dialog.GroupingStrategy');
+
+  /**
+   * Groups images by hour
+   *
+   * @purpose  GroupingStrategy interface implementation
+   */
+  class GroupByHourStrategy extends Object implements GroupingStrategy {
+  
+    /**
+     * Returns group for a given album image.
+     *
+     * @param   &de.thekid.dialog.AlbumImage
+     * @return  string unique group identifier
+     */
+    public function groupFor($image) { 
+      return $image->exifData->dateTime->toString('Y-m-d H');
+    }
+  
+  } 
+?>
