7d7466082c308277e0f907e1421cd145daabcb12
friebe <friebe@d2cacbed-c0f6-0310-851a-9dad52fc623a>
2007-11-16 17:25:59 +0000

    - Initial release
    git-svn-id: svn+ssh://svn.xp-framework.net/home/svn/xpforge/trunk/projects/www/dialog@9899 d2cacbed-c0f6-0310-851a-9dad52fc623a

---
+++ classes/de/thekid/dialog/cmd/AddSingleShot.class.php
@@ -0,0 +1,120 @@
+<?php
+/* This file is part of the XP framework
+ *
+ * $Id$
+ */
+
+  uses(
+    'de.thekid.dialog.cmd.ImportCommand',
+    'de.thekid.dialog.SingleShot',
+    'de.thekid.dialog.io.ShotProcessor'
+  );
+
+  /**
+   * Adds single shots ("Featured images") to dialog
+   *
+   * @purpose  Command
+   */
+  class AddSingleShot extends ImportCommand {
+    protected
+      $origin           = NULL,
+      $destination      = NULL,
+      $shotStorage     = NULL,
+      $groupingStrategy = NULL,
+      $album            = NULL;
+
+    /**
+     * Returns processor. Overrides base class getProcessor() method.
+     *
+     * @return  de.thekid.dialog.io.ImageProcessor
+     */
+    protected function getProcessor() {
+      $processor= new ShotProcessor();
+      $processor->detailDimensions= array(619, 347);
+      return $processor;
+    }
+
+    /**
+     * Set origin file
+     *
+     * @param   string file
+     */
+    #[@arg(position= 0)]
+    public function setOrigin($file) {
+      $this->origin= new File($file);
+      if (!$this->origin->exists()) {
+        throw new FileNotFoundException('File "'.$file.'" does not exist');
+      }
+      
+      // Normalize name
+      $fileName= substr($origin->getFilename(), 0, strpos($origin->getFilename(), '.'));
+      $shotName= $this->normalizeName($fileName);
+      $this->out->writeLine('===> Adding shot "', $shotName, '" from ', $this->origin);
+      
+      // Create destination folder if not already existant
+      $this->destination= new Folder(self::SHOTS_FOLDER);
+      $this->processor->setOutputFolder($this->destination);
+      
+      // Check if the album already exists
+      $this->shotStorage= new File(self::DATA_FOLDER.$shotName.'.dat');
+      if ($this->shotStorage->exists()) {
+        $this->out->writeLine('---> Found existing shot');
+        $this->shot= unserialize(FileUtil::getContents($this->shotStorage));
+      } else {
+        $this->out->writeLine('---> Creating new shot');
+        $this->shot= new SingleShot();
+        $this->shot->setName($shotName);
+      }        
+
+      // Read the introductory text from description.txt if existant
+      if (is_file($df= $origin->getPath().$fileName.DIRECTORY_SEPARATOR.'.txt')) {
+        $this->shot->setDescription(file_get_contents($df));
+      }
+    }
+    
+    /**
+     * Set album's title. If no title is given and the album did not 
+     * previously exist, uses the origin folder's directory name.
+     *
+     * @param   string title default NULL
+     */
+    #[@arg]
+    public function setTitle($title= NULL) {
+      if (!$title && !$this->shot->getTitle()) {
+        $this->shot->setTitle($this->origin->dirname);
+      } else {
+        $this->shot->setTitle($title);
+      }
+      $this->out->writeLine('---> Title "', $this->shot->getTitle(), '"');
+    }
+
+    /**
+     * Set album's creation date. If no date is given and the album did not 
+     * previously exist, uses the origin folder's creation date.
+     *
+     * @param   string date default NULL
+     */
+    #[@arg]
+    public function setCreatedAt($date= NULL) {
+      if (!$date && !$this->shot->getCreatedAt()) {
+        $this->shot->setDate(new Date($this->origin->createdAt()));
+      } else {
+        $this->shot->setDate(new Date($date));
+      }
+      $this->out->writeLine('---> Created ', $this->shot->getCreatedAt());
+    }
+    
+    /**
+     * Import
+     *
+     */
+    protected function doImport() {
+      $image= $this->processor->albumImageFor($this->origin->getURI());
+      $this->processMetaData($image, $this->shot);
+      $this->shot->setImage($image);
+      
+      // Save shot
+      FileUtil::setContents($this->shotStorage, serialize($this->shot));
+    }
+  }
+?>
---
+++ classes/de/thekid/dialog/cmd/ImportCommand.class.php
@@ -0,0 +1,163 @@
+<?php
+/* This file is part of the XP framework
+ *
+ * $Id$
+ */
+
+  uses(
+    'util.cmd.Command',
+    'util.Date',
+    'io.Folder',
+    'io.File',
+    'io.FileUtil',
+    'io.collections.FileCollection',
+    'io.collections.iterate.FilteredIOCollectionIterator',
+    'io.collections.iterate.ExtensionEqualsFilter',
+    'img.filter.ConvolveFilter',
+    'de.thekid.dialog.Topic',
+    'de.thekid.dialog.IEntry',
+    'de.thekid.dialog.AlbumImage',
+    'de.thekid.dialog.io.ImageProcessor',
+    'de.thekid.dialog.io.IndexCreator'
+  );
+
+  /**
+   * Base class for importing entries into dialog.
+   *
+   * @see      xp://de.thekid.dialog.cmd.AddAlbum
+   * @see      xp://de.thekid.dialog.cmd.AddSingleShot
+   * @see      xp://de.thekid.dialog.cmd.AddCollection
+   * @purpose  Command
+   */
+  abstract class ImportCommand extends Command {
+    const
+      IMAGE_FOLDER      = 'doc_root/albums/',
+      SHOTS_FOLDER      = 'doc_root/shots/',
+      DATA_FOLDER       = 'data/',
+      TOPICS_FOLDER     = 'data/topics/';
+    
+    const
+      HIGHLIGHTS_MAX    = 5,
+      ENTRIES_PER_PAGE  = 5;
+      
+    protected
+      $processor        = NULL,
+      $topics           = array();
+
+    /**
+     * Constructor - initializes image processor.
+     *
+     */
+    public function __construct() {
+      $this->processor= $this->getProcessor();
+      $this->processor->fullDimensions= array(800, 600);
+      $this->processor->addFilter(new ConvolveFilter(
+        new Kernel('[[-1, -1, -1], [-1, 16, -1], [-1, -1, -1]]'),
+        8,
+        0
+      ));
+    }
+    
+    /**
+     * Returns processor
+     *
+     * @return  de.thekid.dialog.io.ImageProcessor
+     */
+    protected function getProcessor() {
+      return new ImageProcessor();
+    }
+
+    /**
+     * Normalize name to create a URL-friendly representation
+     *
+     * @param   string name
+     * @return  string normalized
+     */
+    protected function normalizeName($name) {
+      return preg_replace('/[^a-z0-9-]/i', '_', $name);
+    }
+
+    /**
+     * Process meta data
+     *
+     * @param   de.thekid.dialog.AlbumImage image
+     * @param   de.thekid.dialog.IEntry origin
+     */
+    protected function processMetaData(AlbumImage $image, IEntry $origin) {
+    
+      // Ensure image date is always present, fall back to origin's date
+      // if necessary
+      if (!$image->exifData->dateTime) {
+        $image->exifData->dateTime= $origin->getDate();
+      }
+
+      // Extract topics form IPTC keywords if available
+      if (!($iptc= $image->getIptcData())) return;
+      
+      foreach ($iptc->getKeywords() as $keyword) {
+        $normalized= $this->normalizeName($keyword);
+        if (!isset($this->topics[$normalized])) {
+          $topic= new File(self::DATA_FOLDER.'topics/'.$normalized.'.dat');
+          if ($topic->exists()) {
+            $this->topics[$normalized]= unserialize(FileUtil::getContents($topic));
+            $this->out->writeLine('     >> Found existing topic for ', $keyword);
+          } else {
+            $this->out->writeLine('     >> Creating new topic for ', $keyword);
+            $this->topics[$normalized]= new Topic();
+            $this->topics[$normalized]->setName($normalized);
+            $this->topics[$normalized]->setTitle($keyword);
+            $this->topics[$normalized]->setCreatedAt($origin->getDate());
+          }
+        }
+        $this->topics[$normalized]->addImage($image, $origin->getName());
+      }
+    }
+    
+    /**
+     * Import
+     *
+     */
+    protected abstract function doImport();
+    
+    /**
+     * Main runner method
+     *
+     */
+    public function run() {
+      $this->out->writeLine('===> Starting import at ', date('r'));
+      $this->doImport();
+      
+      // Save topics
+      foreach ($this->topics as $normalized => $t) {
+        FileUtil::setContents(new File(self::TOPICS_FOLDER.$normalized.'.dat'), serialize($t));
+      }
+
+      // Regenerate indexes
+      $index= IndexCreator::forFolder(new Folder(self::DATA_FOLDER));
+      $index->setEntriesPerPage(self::ENTRIES_PER_PAGE);
+      $index->regenerate();
+
+      // Generate topics
+      for (
+        $it= new FilteredIOCollectionIterator(new FileCollection(self::TOPICS_FOLDER), new ExtensionEqualsFilter('.dat'));
+        $it->hasNext();
+      ) {
+        $entry= basename($it->next()->getURI());
+        $entries[$entry]= 'topics/'.basename($entry, '.dat');
+      }
+      ksort($entries);
+      for ($i= 0, $s= sizeof($entries); $i < $s; $i+= self::ENTRIES_PER_PAGE) {
+        FileUtil::setContents(
+          new File(self::DATA_FOLDER.'topics_'.($i / self::ENTRIES_PER_PAGE).'.idx'), 
+          serialize(array(
+            'total'   => $s, 
+            'perpage' => self::ENTRIES_PER_PAGE,
+            'entries' => array_slice($entries, $i, self::ENTRIES_PER_PAGE)
+          ))
+        );
+      }
+
+      $this->out->writeLine('===> Finished at ', date('r'));
+    }
+  }
+?>
